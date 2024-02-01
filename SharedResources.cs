a6f211eb8b67e8cc1158ac047794149ad7f98454
fahri.cigdem <fahri.cigdem@atsonline.de>
2024-02-01 16:13:00 +0100

    -Use a static resource in XAML

---
+++ SharedResources.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XAMLMarkup
+{
+    static class SharedResources
+    {
+        public static readonly Color FontColor = Color.FromRgb(0, 0, 0xFF);
+        public static readonly Color BackgroundColor = Color.FromRgb(0, 200, 0);
+    }
+}
