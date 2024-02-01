ba027239d8416e3090404ac22911f5f1221a0dc6
fahri.cigdem <fahri.cigdem@atsonline.de>
2024-02-01 15:19:24 +0100

    - use XAML for UI

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,18 +1,34 @@
-﻿using System.Diagnostics;
+﻿using Microsoft.Maui.Controls;
+using System.Diagnostics;
 
 namespace XAMLMarkup
 {
     public partial class MainPage : ContentPage
     {
+        string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
 
-        
         public MainPage()
         {
             InitializeComponent();
+            if (File.Exists(_fileName))
+            {
+                editor.Text = File.ReadAllText(_fileName);
+            }
         }
 
-      
-
+        private void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            Debug.WriteLine("> Save button is clicked <");
+            File.WriteAllText(_fileName, editor.Text);
+        }
+        private void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            Debug.WriteLine("> Delete button is clicked <");
+            if (File.Exists(_fileName)) { File.Delete(_fileName); }
+            File.Delete(_fileName);
+            editor.Text = string.Empty;
+            
+        }
     }
 
 }
